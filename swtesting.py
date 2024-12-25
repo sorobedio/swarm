3b75165006f8885b1626f837e7db42a0fe3351d2
sorobedio <sorobedio@ieee.org>
2024-12-25 20:39:00 +0900

    updating

--- swtesting.py
+++ swtesting.py
@@ -1679,5 +1679,5 @@ if __name__ == "__main__":
     # Output the best-found expert (global best)
     torch.save(weights, f"./particles/mmlu_swarm_weights_final.pt")
     # print("Best-found weights (global best):", global_best)
-    torch.save(global_best, f"./wdata/mmlu_pro_swarm_global_best_top_1.pt")
+    torch.save(global_best, f"./psrticles/mmlu_swarm_global_best_top_1.pt")
     print("Utility of best-found weights:", utility_function(global_best.unsqueeze(0), layers))
