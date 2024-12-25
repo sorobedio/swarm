248bc4d23f4fc137e0f68dc855a23d0605ac5a30
sorobedio <sorobedio@ieee.org>
2024-12-25 17:29:30 +0900

    updating

--- vae_testing.py
+++ vae_testing.py
@@ -823,14 +823,14 @@ if __name__=='__main__':
     save_dev_flag = args.save_dev_flag
     only_one_or_two = args.only_one_or_two
 
-    # wd = torch.load('wdata/llama3_mmlu_swarm_weights_.pt')
+    wd = torch.load('wdata/llama3_mmlu_swarm_weights_.pt')
     # wd = torch.load('wdata/sampled_weights_vae_norm.pt')
-    wd = torch.load('wdata/mdt_sampled_weights_25_norm_gem.pt')
+    # wd = torch.load('wdata/mdt_sampled_weights_25_norm_gem.pt')
 
 
     wacc = []
-    weights =wd['gemma-7b-it']
-    # weights = wd
+    # weights =wd['gemma-7b-it']
+    weights = wd
     n= weights.shape[0]
     utilities =[]
     for i in range(n):
@@ -852,8 +852,8 @@ if __name__=='__main__':
         utilities.append(results)
         print(results)
         # # print('-----evaluated======================================')
-        # acc =evaluate_test(model, eval_type, dataset, gpu_id, base_model="google/gemma-7b-it", only_one_or_two=None,
-        #               obj4_save_generation=False)
-        # print(acc*100.0)
-    torch.save(utilities, 'wdata/utilities_mdt_norm_mmlu_pro.pt')
+        acc =evaluate_test(model, eval_type, dataset, gpu_id, base_model="google/gemma-7b-it", only_one_or_two=None,
+                      obj4_save_generation=False)
+        print(acc*100.0)
+    # torch.save(utilities, 'wdata/utilities_mdt_norm_mmlu_pro.pt')
 
