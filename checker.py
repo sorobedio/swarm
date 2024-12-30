2a45c221a3bb249a04100fd3b9ffa547e115906d
sorobedio <sorobedio@ieee.org>
2024-12-31 01:37:13 +0900

    updating

--- checker.py
+++ checker.py
@@ -2,6 +2,14 @@ import os
 from transformers import AutoModelForCausalLM
 import torch
 
+for model_name in ["code_alpaca", "cot", "flan_v2", "gemini_alpaca", "lima", "oasst1", "open_orca", "science", "sharegpt", "wizardlm"]:
+    if os.path.exists(model_name):
+        continue
+    model = AutoModelForCausalLM.from_pretrained("bunsenfeng/"+model_name)
+    print(model)
+    exit()
+    model.save_pretrained(model_name)
+
 model_name = "code_alpaca"
 model = AutoModelForCausalLM.from_pretrained("bunsenfeng/"+model_name)
 std = model.state_dict()
