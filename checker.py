1fc1a3c2010281db5085e479cc23d127083b5bd3
sorobedio <sorobedio@ieee.org>
2024-12-31 12:16:45 +0900

    updating

--- checker.py
+++ checker.py
@@ -2,23 +2,38 @@ import os
 from transformers import AutoModelForCausalLM
 import torch
 
-for model_name in ["code_alpaca", "cot", "flan_v2", "gemini_alpaca", "lima", "oasst1", "open_orca", "science", "sharegpt", "wizardlm"]:
-    if os.path.exists(model_name):
-        continue
-    model = AutoModelForCausalLM.from_pretrained("bunsenfeng/"+model_name)
-    print(model)
-    exit()
-    model.save_pretrained(model_name)
 
-model_name = "code_alpaca"
-model = AutoModelForCausalLM.from_pretrained("bunsenfeng/"+model_name)
-std = model.state_dict()
-weights =[]
-for key in std.keys():
-    if 'lora' in key:
-        w = std[key].reshape(-1)
-        print(f'---params:{key}---{w.shape}---')
-        weights.append(w)
-print(len(weights))
-w = torch.cat(weights, dim=-1)
-print(w.shape)
+
+
+
+if __name__ == "__main__":
+    model_names = ["code_alpaca", "cot", "flan_v2", "gemini_alpaca", "lima", "oasst1", "open_orca", "science", "sharegpt", "wizardlm"]
+    vect_weights={}
+    weights ={}
+    wl =[]
+    for model_name in model_names:
+        model = AutoModelForCausalLM.from_pretrained("bunsenfeng/"+model_name)
+        std = model.state_dict()
+        for key in std.keys():
+            if 'lora' in key:
+                w = std[key].reshape(1,-1)
+                # vect_weights[key]=w
+                wl.append(w)
+        weights[model_name]=torch.cat(wl, dim=-1)
+
+
+
+
+
+    model_name = "code_alpaca"
+    model = AutoModelForCausalLM.from_pretrained("bunsenfeng/"+model_name)
+    std = model.state_dict()
+    weights =[]
+    for key in std.keys():
+        if 'lora' in key:
+            w = std[key].reshape(-1)
+            print(f'---params:{key}---{w.shape}---')
+            weights.append(w)
+    print(len(weights))
+    w = torch.cat(weights, dim=-1)
+    print(w.shape)
