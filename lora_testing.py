43e4e56828a06465efc33e1f00599cd032cd6829
sorobedio <sorobedio@ieee.org>
2024-12-31 14:00:56 +0900

    updating

--- lora_testing.py
+++ lora_testing.py
@@ -740,7 +740,7 @@ def get_parser():
                         help="Dataset to use for evaluation")
     parser.add_argument("--gpu_id", type=int, default=0,
                         help="ID of GPU to use")
-    parser.add_argument("--base_model", type=str, default="google/gemma-7b",
+    parser.add_argument("--base_model", type=str, default="google/gemma-7b-it",
                         help="Base model to use")
     parser.add_argument("--save_dev_flag", action="store_true", default=False,
                         help="Whether to save development data")
@@ -803,7 +803,7 @@ if __name__=='__main__':
     print('=============loading model================')
 
 
-    model_id = "google/gemma-7b-it"
+    base_model="google/gemma-7b-it"
     #
     # tokenizer = AutoTokenizer.from_pretrained(model_id)
     # model = AutoModelForCausalLM.from_pretrained(model_id,
