edc078440c2a38bfb129e7c545705b3dea240105
sorobedio <sorobedio@ieee.org>
2024-12-31 12:30:32 +0900

    updating

--- checker.py
+++ checker.py
@@ -19,21 +19,45 @@ if __name__ == "__main__":
                 w = std[key].reshape(1,-1)
                 # vect_weights[key]=w
                 wl.append(w)
-        weights[model_name]=torch.cat(wl, dim=-1)
+        ws= torch.cat(wl, dim=-1)
+        print(f'---params:{model_name}---{ws.shape}---')
+        weights[model_name]=ws
 
 
-
-
-
-    model_name = "code_alpaca"
-    model = AutoModelForCausalLM.from_pretrained("bunsenfeng/"+model_name)
-    std = model.state_dict()
-    weights =[]
-    for key in std.keys():
-        if 'lora' in key:
-            w = std[key].reshape(-1)
-            print(f'---params:{key}---{w.shape}---')
-            weights.append(w)
+    particles_now=len(model_names)
+    initial_experts_num=20
+    for i in range(initial_experts_num - particles_now):
+        parent_1 = random.choice(model_names)
+        parent_2 = random.choice(model_names)
+        while parent_1 == parent_2:
+            parent_2 = random.choice(model_names)
+        w_1 = random.random() * 2  # half interpolation, half extrapolation
+        w_2 = 1 - w_1
+        p1 = weights[parent_1]
+        p2 = weights[parent_2]
+        pm = w_1 * p1 + w_2 * p2
+        k= parent_1+'_'+parent_2
+        weights[k]=pm
+    torch.save(weights, "../Datssets/llmdata/gemina_lora_weights.pt")
     print(len(weights))
-    w = torch.cat(weights, dim=-1)
-    print(w.shape)
+
+    exit(0)
+    
+
+
+
+
+
+
+    # model_name = "code_alpaca"
+    # model = AutoModelForCausalLM.from_pretrained("bunsenfeng/"+model_name)
+    # std = model.state_dict()
+    # weights =[]
+    # for key in std.keys():
+    #     if 'lora' in key:
+    #         w = std[key].reshape(-1)
+    #         print(f'---params:{key}---{w.shape}---')
+    #         weights.append(w)
+    # print(len(weights))
+    # w = torch.cat(weights, dim=-1)
+    # print(w.shape)
