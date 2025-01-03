f860fd97dcdad6d1ca4eac5f8245a113312a28ab
sorobedio <sorobedio@ieee.org>
2025-01-03 11:58:15 +0900

    updating

--- dtrainer.py
+++ dtrainer.py
@@ -337,7 +337,7 @@ if __name__ == "__main__":
     # # Combine schedulers using SequentialLR
     # scheduler = torch.optim.lr_scheduler.SequentialLR(optimizer, schedulers=[scheduler_warmup, scheduler_cosine],
     #                          milestones=[warmup_iters])
-    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=400, eta_min=1e-7, last_epoch=-1)
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=10000, eta_min=1e-7, last_epoch=-1)
     criterion = model.loss
     # train(model, optimizer, args.n_epochs, traindataloader, testdataloader)
     train(model, optimizer, args.n_epochs, traindataloader)
