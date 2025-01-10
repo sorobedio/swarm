bf4446f0a5d572aa2fb4d32dfe39cc38c47e0456
sorobedio <sorobedio@ieee.org>
2025-01-10 11:41:33 +0900

    updating

--- ldtrainer.py
+++ ldtrainer.py
@@ -248,7 +248,7 @@ def train(model, optimizer, n_epochs, traindataloader, testdataloader=None):
         if bloss > tloss:
             bloss = tloss
             print(f'saving best training loss is:{bloss}')
-            torch.save(model, os.path.join(args.save_path,f'llama_full_chunk_simple.pth'))
+            torch.save(model, os.path.join(args.save_path,f'lora_hunk_base.pth'))
             # torch.save(model.state_dict(), os.path.join(args.save_path, f'llama_3_1_8B_models_ffn_l-30.ckpt'))
         print(f'best training loss is:{bloss}  lr={curr_lr}')
 
