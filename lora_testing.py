a888d8bf75e6a22377636fb96c2828c53a2b369a
sorobedio <sorobedio@ieee.org>
2025-01-12 13:42:44 +0900

    updating

--- lora_testing.py
+++ lora_testing.py
@@ -831,7 +831,7 @@ if __name__=='__main__':
 
     # model_names = ["code_alpaca", "cot", "flan_v2", "gemini_alpaca", "lima", "oasst1", "open_orca", "science",
     #                "sharegpt", "wizardlm"]
-    wd = torch.load("../Datasets/llmdata/gemina7b_it_lora_weights.pt")
+    wd = torch.load("../Datasets/gemina7b_it_lora_weights.pt")
     # model_names=list(wd.keys())
     base_model = "google/gemma-7b-it"
     modelist=list(wd.keys())[:1]
