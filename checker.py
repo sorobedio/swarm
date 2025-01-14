cfc2778c173a73efc1204020d60f7b257876612e
sorobedio <sorobedio@ieee.org>
2025-01-14 16:53:29 +0900

    updating

--- checker.py
+++ checker.py
@@ -18,21 +18,22 @@ if __name__ == "__main__":
     # # Generate a random float in the range (epsilon, 1 - epsilon)
     # scalar = random.uniform(epsilon, 1 - epsilon)
 
-    # for model_name in model_names:
-    #     adapter_id="bunsenfeng/"+model_name
-    #     model = AutoModelForCausalLM.from_pretrained(base_model, torch_dtype=torch.float16)
-    #     model.load_adapter(adapter_id)
-    #     std = model.state_dict()
-    #     wl = []
-    #     for key in std.keys():
-    #         if 'lora' in key:
-    #             w = std[key].reshape(1,-1)
-    #             # vect_weights[key]=w
-    #             print(f'---params:{key}---{w.shape}--{w.dtype}-')
-    #             wl.append(w)
-    #     ws= torch.cat(wl, dim=-1)
-    #     print(f'---params:{model_name}---{ws.shape}--{ws.min()}-{ws.max()}-')
-    #     weights[model_name]=ws
+    for model_name in model_names:
+        adapter_id="bunsenfeng/"+model_name
+        model = AutoModelForCausalLM.from_pretrained(base_model, torch_dtype=torch.float16)
+        model.load_adapter(adapter_id)
+        std = model.state_dict()
+        wl = []
+        for key in std.keys():
+            if 'lora' in key:
+                w = std[key].reshape(1,-1)
+                # vect_weights[key]=w
+                print(f'---params:{key}---{w.shape}--{w.dtype}-')
+                wl.append(w)
+        ws= torch.cat(wl, dim=-1)
+        print(f'---params:{model_name}---{ws.shape}--{ws.min()}-{ws.max()}-')
+        weights[model_name]=ws
+    exit()
     neweights ={}
     weights= torch.load('wdata/reconstruct_lora_weights.pt')
     model_names = list(weights)
