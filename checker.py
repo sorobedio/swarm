af3a7c6e715899bbe3fe253567a3d8ec11e875e3
sorobedio <sorobedio@ieee.org>
2025-01-14 13:05:43 +0900

    updating

--- checker.py
+++ checker.py
@@ -18,40 +18,41 @@ if __name__ == "__main__":
     # # Generate a random float in the range (epsilon, 1 - epsilon)
     # scalar = random.uniform(epsilon, 1 - epsilon)
 
-    for model_name in model_names:
-        adapter_id="bunsenfeng/"+model_name
-        model = AutoModelForCausalLM.from_pretrained(base_model, torch_dtype=torch.float16)
-        model.load_adapter(adapter_id)
-        std = model.state_dict()
-        wl = []
-        for key in std.keys():
-            if 'lora' in key:
-                w = std[key].reshape(1,-1)
-                # vect_weights[key]=w
-                print(f'---params:{key}---{w.shape}--{w.dtype}-')
-                wl.append(w)
-        ws= torch.cat(wl, dim=-1)
-        print(f'---params:{model_name}---{ws.shape}--{ws.min()}-{ws.max()}-')
-        weights[model_name]=ws
+    # for model_name in model_names:
+    #     adapter_id="bunsenfeng/"+model_name
+    #     model = AutoModelForCausalLM.from_pretrained(base_model, torch_dtype=torch.float16)
+    #     model.load_adapter(adapter_id)
+    #     std = model.state_dict()
+    #     wl = []
+    #     for key in std.keys():
+    #         if 'lora' in key:
+    #             w = std[key].reshape(1,-1)
+    #             # vect_weights[key]=w
+    #             print(f'---params:{key}---{w.shape}--{w.dtype}-')
+    #             wl.append(w)
+    #     ws= torch.cat(wl, dim=-1)
+    #     print(f'---params:{model_name}---{ws.shape}--{ws.min()}-{ws.max()}-')
+    #     weights[model_name]=ws
 
-
-    # particles_now=len(model_names)
-    # initial_experts_num=20 ##51380224
-    # for i in range(initial_experts_num - particles_now):
-    #     parent_1 = random.choice(model_names)
-    #     parent_2 = random.choice(model_names)
-    #     while parent_1 == parent_2:
-    #         parent_2 = random.choice(model_names)
-    #     # w_1 = random.random() * 2  # half interpolation, half extrapolation
-    #     w_1 = random.uniform(epsilon, 1 - epsilon)
-    #     w_2 = 1 - w_1
-    #     p1 = weights[parent_1]
-    #     p2 = weights[parent_2]
-    #     pm = w_1 * p1 + w_2 * p2
-    #     k= parent_1+'_'+parent_2
-    #     weights[k]=pm
-    # torch.save(weights, "../Datasets/gemina7b_it_lora_weights_indiv.pt")
-    # print(len(weights))
+    weights= torch.load('wdata/reconstruct_lora_weights.pt')
+    model_names = list(weights)
+    particles_now=len(model_names)
+    initial_experts_num=30 ##51380224
+    for i in range(initial_experts_num - particles_now):
+        parent_1 = random.choice(model_names)
+        parent_2 = random.choice(model_names)
+        while parent_1 == parent_2:
+            parent_2 = random.choice(model_names)
+        # w_1 = random.random() * 2  # half interpolation, half extrapolation
+        w_1 = random.uniform(epsilon, 1 - epsilon)
+        w_2 = 1 - w_1
+        p1 = weights[parent_1]
+        p2 = weights[parent_2]
+        pm = w_1 * p1 + w_2 * p2
+        k= parent_1+'_t_'+parent_2
+        weights[k]=pm
+    torch.save(weights, "../Datasets/llmdata/gemina7b_it_lora_weights_recon_ext.pt")
+    print(len(weights))
 
     exit(0)
 
