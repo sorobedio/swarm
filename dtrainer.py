a61cf5ca3917cff8c577a1b366d66afa35df8ab5
sorobedio <sorobedio@ieee.org>
2025-01-18 02:18:09 +0900

    updating

--- dtrainer.py
+++ dtrainer.py
@@ -316,9 +316,9 @@ if __name__ == "__main__":
     #     "epochs": 20000,
     #     "batch_size": 16,
     #     "learning_rate": 0.001,
-    # })
+    # })_chunk
     # seed_everything(seed=1234)
-    writer = SummaryWriter(log_dir="first_100_full_llama/tensorboard_encod")
+    writer = SummaryWriter(log_dir="first_128_full_llama/tensorboard_encod")
     now = datetime.datetime.now().strftime("%Y-%m-%dT%H-%M-%S")
     # sys.path.append(os.getcwd())
     parser = get_parser()
