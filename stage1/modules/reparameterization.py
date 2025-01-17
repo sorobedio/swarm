46959e60e1cc59c2aae45ab77de69dc26f64e0d4
sorobedio <sorobedio@ieee.org>
2025-01-17 14:39:29 +0900

    updating

--- stage1/modules/reparameterization.py
+++ stage1/modules/reparameterization.py
@@ -24,7 +24,7 @@ class DiracDistribution(AbstractDistribution):
 class DiagonalGaussianDistribution(object):
     def __init__(self, parameters, deterministic=False):
         self.parameters = parameters
-        self.mean, self.logvar = torch.chunk(parameters, 2, dim=1)
+        self.mean, self.logvar = torch.chunk(parameters, 2, dim=-1)
         self.logvar = torch.clamp(self.logvar, -30.0, 30.0)
         self.deterministic = deterministic
         self.std = torch.exp(0.5 * self.logvar)
