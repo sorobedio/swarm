d55e68013e35f344d7a45342a5cb0092aa522931
sorobedio <sorobedio@ieee.org>
2025-01-19 02:24:17 +0900

    updating

--- lineardtrainer.py
+++ lineardtrainer.py
@@ -242,7 +242,7 @@ def train(model, optimizer, n_epochs, traindataloader, testdataloader=None):
         if bloss > tloss:
             bloss = tloss
             print(f'saving best training loss is:{bloss}')
-            torch.save(model, os.path.join(args.save_path,f'full_llama_model_chunk_full_linear_withlearn.pth'))
+            torch.save(model, os.path.join(args.save_path,f'full_llama_model_chunk_full_linear_withlearn_full.pth'))
             # torch.save(model.state_dict(), os.path.join(args.save_path, f'llama_3_1_8B_models_ffn_l-30.ckpt'))
         print(f'best training loss is:{bloss}  lr={curr_lr}')
 
