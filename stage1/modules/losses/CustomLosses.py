c63c79b5712d8b9ae9a18f4f9d96af4dfdecf6f8
sorobedio <sorobedio@ieee.org>
2025-01-22 00:29:28 +0900

    updating

--- stage1/modules/losses/CustomLosses.py
+++ stage1/modules/losses/CustomLosses.py
@@ -1,6 +1,6 @@
 import torch
 import torch.nn as nn
-
+import torch.nn.functional as F
 
 class NopaddingLoss(nn.Module):
     def __init__(self, logvar_init=0.0, kl_weight=1.0, pad_value=0):
@@ -77,12 +77,30 @@ class Myloss(nn.Module):
         nll_loss = torch.sum(nll_loss) / nll_loss.shape[0]
         kl_loss = posteriors.kl()
         kl_loss = torch.sum(kl_loss) / kl_loss.shape[0]
-        loss = weighted_nll_loss + self.kl_weight * kl_loss
+        # loss = weighted_nll_loss + self.kl_weight * kl_loss
+        loss = F.mse_loss(inputs.contiguous(), reconstructions.contiguous()) +self.kl_weight * kl_loss.mean()
 
-        log = {"{}/total_loss".format(split): loss.clone().detach().mean(),"{}/logvar".format(split): self.logvar.detach(),
+        log = {"{}/total_loss".format(split): loss.clone().detach(),"{}/logvar".format(split): self.logvar.detach(),
                "{}/kl_loss".format(split): kl_loss.detach().mean(), "{}/nll_loss".format(split): nll_loss.detach().mean(),
                "{}/rec_loss".format(split): rec_loss.detach().mean(),
                }
+        #
+        # nll_loss = rec_loss
+        # weighted_nll_loss = nll_loss
+        # if weights is not None:
+        #     weighted_nll_loss = weights * nll_loss
+        # weighted_nll_loss = torch.sum(weighted_nll_loss) / weighted_nll_loss.shape[0]
+        # nll_loss = torch.sum(nll_loss) / nll_loss.shape[0]
+        # kl_loss = posteriors.kl()
+        # kl_loss = torch.sum(kl_loss) / kl_loss.shape[0]
+        # loss = weighted_nll_loss + self.kl_weight * kl_loss
+        #
+        # log = {"{}/total_loss".format(split): loss.clone().detach().mean(),
+        #        "{}/logvar".format(split): self.logvar.detach(),
+        #        "{}/kl_loss".format(split): kl_loss.detach().mean(),
+        #        "{}/nll_loss".format(split): nll_loss.detach().mean(),
+        #        "{}/rec_loss".format(split): rec_loss.detach().mean(),
+        #        }
         return loss, log
 
 
