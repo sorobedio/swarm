d5b0bb7289cd0ec25431b448a9d0f0c5fa61bf3c
sorobedio <sorobedio@ieee.org>
2025-01-21 09:33:34 +0900

    updating

--- zoodatasets/cnn_chunkdataset.py
+++ zoodatasets/cnn_chunkdataset.py
@@ -130,7 +130,7 @@ class ZooDataset(Dataset):
             keys = list(data)
             # keys.remove('layernorm.weight')
             # keys = ['sharegpt_cot', 'gemini_alpaca_sharegpt']
-            # keys =keys[:14]
+            keys =keys[:56]
             # print(keys)
 
             for k in keys:
