4ea821908d86e0937c593385a59af2efed52c62e
sorobedio <sorobedio@ieee.org>
2025-01-22 12:18:37 +0900

    updating

--- layer_search.py
+++ layer_search.py
@@ -430,7 +430,7 @@ if __name__ == "__main__":
     #
     # chunk_size =2362368
     # chunk_size = 1100416
-    chunk_size=1048576
+    chunk_size=65536
     # 8030261248
     # chunk_size = 16384
     scale = 0.1
@@ -448,7 +448,7 @@ if __name__ == "__main__":
     # autoencoder = torch.load('./autocheckpoints/llama_model_chunk_full_block_first_full_v2.pth', map_location='cpu')
     # autoencoder = torch.load('./autocheckpoints/llama_model_chunk_full_block_7first.pth', map_location='cpu')
     # torch.save(autoencoder.state_dict(), f'checkpoints/stage1/base_chunk_llama_v1.ckpt')
-    autoencoder = torch.load('./autocheckpoints/llama_model_1b_tf_block_.pth', map_location='cpu')
+    autoencoder = torch.load('./autocheckpoints/llama_model_1b_tf_block_full.pth', map_location='cpu')
     # torch.save(autoencoder.state_dict(), f'checkpoints/stage1/pythia_160m_ffn_44step.ckpt')
 
     # exit()
