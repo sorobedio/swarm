1e66cb067b32c73e8422cd1750f3961a399d1ca9
sorobedio <sorobedio@ieee.org>
2025-01-29 03:17:51 +0900

    updating

--- zoodatasets/weightsdatasets.py
+++ zoodatasets/weightsdatasets.py
@@ -68,7 +68,7 @@ class ZooDataset(Dataset):
         # exit()
         self.data = data.detach().cpu()
         print(f'===============dataset size=={data.shape}======max={data.max()}======={data.min()}==========')
-        exit()
+        # exit()
 
     def __len__(self):
         return len(self.data)
