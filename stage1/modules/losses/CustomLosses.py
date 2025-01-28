663d11705d837c56262ae51abd52159342e2a9a8
sorobedio <sorobedio@ieee.org>
2025-01-28 18:40:08 +0900

    updating

--- stage1/modules/losses/CustomLosses.py
+++ stage1/modules/losses/CustomLosses.py
@@ -69,16 +69,16 @@ class Myloss(nn.Module):
         rec_loss = (inputs.contiguous() - reconstructions.contiguous())**2
         self.logvar.data.clamp_(min=-30, max=30)
 
-        nll_loss = rec_loss / (torch.exp(self.logvar)*2) + self.logvar*0.5
-        # nll_loss = rec_loss
+        # nll_loss = rec_loss / (torch.exp(self.logvar)*2) + self.logvar*0.5
+        nll_loss = rec_loss
         weighted_nll_loss = nll_loss
         if weights is not None:
             weighted_nll_loss = weights*nll_loss
-        weighted_nll_loss = torch.sum(weighted_nll_loss) / weighted_nll_loss.shape[0]
-        nll_loss = torch.sum(nll_loss) / nll_loss.shape[0]
+        # weighted_nll_loss = torch.sum(weighted_nll_loss) / weighted_nll_loss.shape[0]
+        # nll_loss = torch.sum(nll_loss) / nll_loss.shape[0]
         kl_loss = posteriors.kl()
-        kl_loss = torch.sum(kl_loss) / kl_loss.shape[0]
-        loss = weighted_nll_loss + self.kl_weight * kl_loss
+        # kl_loss = torch.sum(kl_loss) / kl_loss.shape[0]
+        loss = weighted_nll_loss.mean() + self.kl_weight * kl_loss.mean()
         # loss = 100*F.mse_loss(inputs.contiguous(), reconstructions.contiguous()) +self.kl_weight * kl_loss.mean()
 
         log = {"{}/total_loss".format(split): loss.clone().detach().mean(),
@@ -87,23 +87,7 @@ class Myloss(nn.Module):
                "{}/nll_loss".format(split): nll_loss.detach().mean(),
                "{}/rec_loss".format(split): rec_loss.detach().mean(),
                }
-        #
-        # nll_loss = rec_loss
-        # weighted_nll_loss = nll_loss
-        # if weights is not None:
-        #     weighted_nll_loss = weights * nll_loss
-        # weighted_nll_loss = torch.sum(weighted_nll_loss) / weighted_nll_loss.shape[0]
-        # nll_loss = torch.sum(nll_loss) / nll_loss.shape[0]
-        # kl_loss = posteriors.kl()
-        # kl_loss = torch.sum(kl_loss) / kl_loss.shape[0]
-        # loss = weighted_nll_loss + self.kl_weight * kl_loss
-        #
-        # log = {"{}/total_loss".format(split): loss.clone().detach().mean(),
-        #        "{}/logvar".format(split): self.logvar.detach(),
-        #        "{}/kl_loss".format(split): kl_loss.detach().mean(),
-        #        "{}/nll_loss".format(split): nll_loss.detach().mean(),
-        #        "{}/rec_loss".format(split): rec_loss.detach().mean(),
-        #        }
+
         return loss, log
 
 
