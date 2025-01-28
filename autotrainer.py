527322e2e4733d3c222ad9e7fc97f6a54f183c3b
sorobedio <sorobedio@ieee.org>
2025-01-29 00:59:22 +0900

    updating

--- autotrainer.py
+++ autotrainer.py
@@ -199,7 +199,7 @@ import torch
 def train(model, optimizer, n_epochs, traindataloader, testdataloader=None, use_amp=False, args=None):
     if not os.path.exists(args.save_path):
         os.makedirs(args.save_path, exist_ok=True)
-    bloss = 1000.0
+    bloss = 5.0
     btest = 2.0
     scaler = torch.cuda.amp.GradScaler(enabled=use_amp)
 
@@ -237,7 +237,7 @@ def train(model, optimizer, n_epochs, traindataloader, testdataloader=None, use_
         if bloss > tloss:
             bloss = tloss
             print(f'Saving model with best training loss: {bloss:.4f}')
-            torch.save(model, os.path.join(args.save_path, f'miture_hf_model_llama1b_128_auto_.pth'))
+            torch.save(model, os.path.join(args.save_path, f'hf_model_llama1b_1048_auto_.pth'))
 
 
         print(f' Rec_LOSS: {tloss}  Best Training Loss: {bloss:.4f}, LR: {optimizer.param_groups[-1]["lr"]:.6f}')
