d96a7f8112d8b1ba33e8ecfb5ff2c18b1caf234b
sorobedio <sorobedio@ieee.org>
2025-01-30 04:31:28 +0900

    updating

--- conv1d_autotrainer.py
+++ conv1d_autotrainer.py
@@ -38,7 +38,7 @@ from tqdm import tqdm
 
 
 
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 
 
 def get_parser(**parser_kwargs):
@@ -199,7 +199,7 @@ import torch
 def train(model, optimizer, n_epochs, traindataloader, testdataloader=None, use_amp=False, args=None):
     if not os.path.exists(args.save_path):
         os.makedirs(args.save_path, exist_ok=True)
-    bloss = 5.0
+    bloss = 1.0
     btest = 2.0
     scaler = torch.cuda.amp.GradScaler(enabled=use_amp)
 
@@ -334,7 +334,7 @@ if __name__ == "__main__":
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     trainset = ZooDataset(root=args.data,  dataset="joint", split=args.split,
-                          scale=1.0, normalize=None)
+                          scale=0.01, normalize=None)
     # valset = ZooDataset(root=args.data, dataset=args.dataset, split=args.split, normalize=False)
 #0.5
     traindataloader = DataLoader(trainset, shuffle=True, batch_size=64, num_workers=8,
