58c5d4b21fe0c48511ba15c7cb4524550101def8
sorobedio <sorobedio@ieee.org>
2025-01-31 00:30:35 +0900

    updating

--- stage1/models/t-vae.py
+++ stage1/models/t-vae.py
@@ -6,12 +6,20 @@ from stage1.modules.modules import Encoder, Decoder
 from stage1.modules.distributions import DiagonalGaussianDistribution
 from utils.util import instantiate_from_config
 from stage1.modules.losses.CustomLosses import ChunkWiseReconLoss
+import torch.distributions as dist
 
- # Define Log-Cosh Loss
+
+# Define Log-Cosh Loss
 def log_cosh_loss(y_pred, y_true):
     return torch.mean(torch.log(torch.cosh(y_pred - y_true)))
 
-class Autoencoder(nn.Module):
+
+# Define Student's T Latent Regularization Loss
+def student_t_kl_loss(z, df):
+    return torch.mean((df + 1) / 2 * torch.log(1 + (z ** 2) / df))
+
+
+class TAutoencoder(nn.Module):
     def __init__(self,
                  ddconfig,
                  embed_dim,
@@ -23,20 +31,20 @@ class Autoencoder(nn.Module):
                  device='cuda',
                  latent_shape=None,
                  monitor=None,
-                 ):
+                 lambda_recon=1.0,
+                 lambda_latent=0.1):
         super().__init__()
-        self.devices =device
+        self.devices = device
         self.cond_key = cond_key
-        self.learning_rate =  learning_rate
+        self.learning_rate = learning_rate
         self.input_key = input_key
+        self.lambda_recon = lambda_recon
+        self.lambda_latent = lambda_latent
         self.encoder = Encoder(**ddconfig)
         self.decoder = Decoder(**ddconfig)
-        # self.loss = instantiate_from_config(lossconfig)
-        # self.chunk_loss = ChunkWiseReconLoss(step_size=128)
-
 
         assert ddconfig["double_z"]
-        self.quant_conv = torch.nn.Conv2d(2*ddconfig["z_channels"], embed_dim, 1)
+        self.quant_conv = torch.nn.Conv2d(2 * ddconfig["z_channels"], 3*embed_dim, 1)
         self.post_quant_conv = torch.nn.Conv2d(embed_dim, ddconfig["z_channels"], 1)
         self.embed_dim = embed_dim
 
@@ -45,93 +53,73 @@ class Autoencoder(nn.Module):
         if ckpt_path is not None:
             self.init_from_ckpt(ckpt_path, ignore_keys=ignore_keys)
 
-
-
     def xavier_initialize(self, model):
-        """
-        Applies Xavier initialization to all layers in the given model.
-        Args:
-            model (nn.Module): The neural network model to initialize.
-        """
         for name, param in model.named_parameters():
-            if "weight" in name:
-                if param.dim() > 1:  # Only apply to layers with at least 2 dimensions
-                    nn.init.xavier_uniform_(param)
-                    print(f"Xavier initialized: {name}")
+            if "weight" in name and param.dim() > 1:
+                nn.init.xavier_uniform_(param)
             elif "bias" in name:
                 nn.init.zeros_(param)
-                print(f"Bias initialized to zero: {name}")
 
     def init_from_ckpt(self, path, ignore_keys=list()):
         sd = torch.load(path, map_location="cpu")
         if "state_dict" in sd:
             sd = sd["state_dict"]
-        keys = list(sd.keys())
-        for k in keys:
-            for ik in ignore_keys:
-                if k.startswith(ik):
-                    print("Deleting key {} from state_dict.".format(k))
-                    del sd[k]
+        for k in list(sd.keys()):
+            if any(k.startswith(ik) for ik in ignore_keys):
+                del sd[k]
         self.load_state_dict(sd, strict=False)
-        print(f"Restored from {path}")
 
     def encode(self, x):
-
         h = self.encoder(x)
-        z = self.quant_conv(h)
-
-        return z
+        z_params = self.quant_conv(h)
+        mu, logvar, logdf = torch.chunk(z_params, 3, dim=1)
+        df = torch.exp(logdf) + 2.0  # Ensure df > 2 for stability
+        eps = dist.StudentT(df).rsample(mu.shape)  # Sample from Student's T
+        z = mu + torch.exp(0.5 * logvar) * eps  # Reparameterization trick
+        return z, df
 
     def decode(self, z):
         z = self.post_quant_conv(z)
-        dec = self.decoder(z)
-        return dec
+        return self.decoder(z)
 
-    def forward(self, input, sample_posterior=True):
+    def forward(self, input):
         if isinstance(input, dict):
             input = input[self.input_key].to(self.devices)
-        z = self.encode(input)
-
+        z, df = self.encode(input)
         dec = self.decode(z)
-        dec = dec.reshape(input.shape)
-        return input, dec
+        return input, dec, z, df
 
     def get_input(self, batch, k):
-        x = batch[k].to(self.devices)
-        return x
+        return batch[k].to(self.devices)
 
     def training_step(self, batch, batch_idx, optimizer_idx):
-
         inputs = self.get_input(batch, self.input_key)
-        inputs, reconstructions= self(inputs)
-        loss = F.mse_loss(reconstructions, inputs, reduction="mean")
-
-
-
+        inputs, reconstructions, z, df = self(inputs)
+        recon_loss = log_cosh_loss(reconstructions, inputs)
+        latent_loss = student_t_kl_loss(z, df)
+        loss = self.lambda_recon * recon_loss + self.lambda_latent * latent_loss
         return loss
 
     def validation_step(self, batch, batch_idx):
         inputs = self.get_input(batch, self.input_key)
-        inputs, reconstructions = self(inputs)
-        loss = F.mse_loss(reconstructions, inputs, reduction="mean")
-
+        inputs, reconstructions, z, df = self(inputs)
+        recon_loss = log_cosh_loss(reconstructions, inputs)
+        latent_loss = student_t_kl_loss(z, df)
+        loss = self.lambda_recon * recon_loss + self.lambda_latent * latent_loss
         return loss
 
     def configure_optimizers(self):
         lr = self.learning_rate
-        opt_ae = torch.optim.Adam(list(self.encoder.parameters())+
-                                  list(self.decoder.parameters())+
-                                  list(self.quant_conv.parameters())+
-                                  list(self.post_quant_conv.parameters()),
-                                  lr=lr, betas=(0.5, 0.9))
-        # opt_disc = torch.optim.Adam(self.loss.discriminator.parameters(),
-        #                             lr=lr, betas=(0.5, 0.9))
+        opt_ae = torch.optim.Adam(
+            list(self.encoder.parameters()) +
+            list(self.decoder.parameters()) +
+            list(self.quant_conv.parameters()) +
+            list(self.post_quant_conv.parameters()),
+            lr=lr, betas=(0.5, 0.9))
         return [opt_ae], []
 
     def get_last_layer(self):
         return self.decoder.conv_out.weight
-
-
 
 
 class AENoDiscModel(Autoencoder):
@@ -160,23 +148,24 @@ class AENoDiscModel(Autoencoder):
     #     self.loss.kl_weight = self.beta_scheduler.get_beta(global_step)
 
     def training_step(self, batch, batch_idx):
-        # if self.is_kl_beta:
-        #     self.beta_scheduling(self.gl_step)
-        # inputs, reconstructions, posterior = self(batch)
         inputs = self.get_input(batch, self.input_key)
-        inputs, reconstructions = self(inputs)
-        # loss = F.smooth_l1_loss(reconstructions, inputs, reduction='mean') * 1000.0
-        loss = F.mse_loss(reconstructions, inputs, reduction="mean")*1000.0
-        # loss = log_cosh_loss(reconstructions, inputs)*1000
+        inputs, reconstructions, z, df = self(inputs)
+        recon_loss  = F.mse_loss(reconstructions, inputs, reduction="mean") * 1000.0
+        # recon_loss = log_cosh_loss(reconstructions, inputs)
+        latent_loss = student_t_kl_loss(z, df)
+        loss = self.lambda_recon * recon_loss + self.lambda_latent * latent_loss
+
+
 
         return loss
 
     def validation_step(self, batch, batch_idx):
         inputs = self.get_input(batch, self.input_key)
-        inputs, reconstructions = self(inputs)
-        # loss = F.mse_loss(reconstructions, inputs, reduction="mean")*1000.0
-        loss = log_cosh_loss(reconstructions, inputs) * 1000
-
+        inputs, reconstructions, z, df = self(inputs)
+        recon_loss = F.mse_loss(reconstructions, inputs, reduction="mean") * 1000.0
+        # recon_loss = log_cosh_loss(reconstructions, inputs)
+        latent_loss = student_t_kl_loss(z, df)
+        loss = self.lambda_recon * recon_loss + self.lambda_latent * latent_loss
         return loss
 
     def configure_optimizers(self):
@@ -210,4 +199,3 @@ class IdentityFirstStage(torch.nn.Module):
 
     def forward(self, x, *args, **kwargs):
         return x
-
