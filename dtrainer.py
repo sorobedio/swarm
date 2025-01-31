c79fc8844a5a6318053ec785f51d15b6b1d93a7a
sorobedio <sorobedio@ieee.org>
2025-01-31 11:24:55 +0900

    updating

--- dtrainer.py
+++ dtrainer.py
@@ -214,7 +214,7 @@ import torch
 def train(model, optimizer, n_epochs, traindataloader, testdataloader=None, use_amp=False, args=None):
     if not os.path.exists(args.save_path):
         os.makedirs(args.save_path, exist_ok=True)
-    bloss = 10.0
+    bloss = 0.1
     btest = 2.0
     scaler = torch.cuda.amp.GradScaler(enabled=use_amp)
 
