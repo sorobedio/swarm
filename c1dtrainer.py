5323a69ff6b544b390a1f50d026f8618199e5973
sorobedio <sorobedio@ieee.org>
2025-02-02 19:19:52 +0900

    updating

--- c1dtrainer.py
+++ c1dtrainer.py
@@ -202,7 +202,7 @@ def nondefault_trainer_args(opt):
 def train(model, optimizer, n_epochs, traindataloader, testdataloader=None):
     if not os.path.exists(args.save_path):
         os.makedirs(args.save_path, exist_ok=True)
-    bloss = 3.0
+    bloss = 50000.0
     btest = 2.0
     cr =[]
     # schedulers = lr_scheduler.CosineAnnealingWarmRestarts(optimizer, 5, 5)
@@ -345,7 +345,7 @@ if __name__ == "__main__":
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     trainset = ZooDataset(root=args.data,  dataset="joint", split=args.split,
-                          scale=1, normalize='z_score')
+                          scale=0.0125, normalize=None)
     # valset = ZooDataset(root=args.data, dataset=args.dataset, split=args.split, normalize=False)
 #0.5
     traindataloader = DataLoader(trainset, shuffle=True, batch_size=32, num_workers=4,
