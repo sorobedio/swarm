12590dd7f56928afeac662652c513840aed50ae7
sorobedio <sorobedio@ieee.org>
2025-02-03 14:11:28 +0900

    updating

--- stage1/models/first_stage_model.py
+++ stage1/models/first_stage_model.py
@@ -83,10 +83,10 @@ class AutoencoderKL(nn.Module):
         print(f"Restored from {path}")
 
     def encode(self, x):
-        print(x.dtype, x.device,x.shape)
+        # print(x.dtype, x.device,x.shape)
         # x =log_transform(x)
         # x = torch.randn
-        h = self.encoder.to(self.device)
+        h = self.encoder(x)
         moments = self.quant_conv(h)
         posterior = DiagonalGaussianDistribution(moments)
         return posterior
