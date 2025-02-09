8da42dfb57a4791c34500886304ddaae5003a8dd
sorobedio <sorobedio@ieee.org>
2025-02-09 21:32:04 +0900

    updating

--- zoodatasets/chunkdatasets.py
+++ zoodatasets/chunkdatasets.py
@@ -84,7 +84,7 @@ class ZooDataset(Dataset):
 
 
     def __len__(self):
-        return 1000  #len(self.data)
+        return len(self.data)
 
     def __getitem__(self, idx):
         if torch.is_tensor(idx):
